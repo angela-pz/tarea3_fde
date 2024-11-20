1cad9992ab6949fcbaf44d0e38bc4304949cc5d2
meripozo <9306433@alumnos.ufv.es>
2024-11-20 16:45:21 +0100

    Hemos fusionado el código de la practica3 con el código de clase

--- ficherot3.py
+++ ficherot3.py
@@ -1,24 +1,81 @@
+import random
 from coppeliasim_zmqremoteapi_client import RemoteAPIClient
+
 import cv2 as cv
 import numpy as np
 client = RemoteAPIClient()
 sim = client.require('sim')
+
 sim.setStepping(True)
+sim.startSimulation()  
 
-myRobot = sim.getObject('/PioneerP3DX')  
-
-sim.startSimulation()
-
+# Definimos el robot que utilizamos, y los elementos necesarios
+My_robot = sim.getObject('/PioneerP3DX') 
 leftMotor = sim.getObject('/PioneerP3DX/leftMotor')  
-rightMotor = sim.getObject('/PioneerP3DX/rightMotor') 
-camera = sim.getObject('/PioneerP3Dx/Camera')
+rightMotor = sim.getObject('/PioneerP3DX/rightMotor')
+camera = sim.getObject('/PioneerP3DX/Camera')
+distanceSensorLeft = sim.getObject('/PioneerP3DX/ultrasonicSensorL') 
+distanceSensorFrontLeft = sim.getObject('/PioneerP3DX/ultrasonicSensorFL') 
+distanceSensorFrontRight = sim.getObject('/PioneerP3DX/ultrasonicSensorFR') 
+distanceSensorRight = sim.getObject('/PioneerP3DX/ultrasonicSensorR') 
 
-sim.setJointTargetVelocity(leftMotor, -0.2)
-sim.setJointTargetVelocity(rightMotor, 0.2)
+# Configuramos la velocidad inicial de nuestro robot
+sim.setJointTargetVelocity(leftMotor, 0.5)  
+sim.setJointTargetVelocity(rightMotor, 0.5)
 
-while True:
+# Distancia a la que detecta la pared
+threshold = 0.2  
+
+while True: 
+    # Detectamos la distancia con los sensores
+    frontState, frontDistance, *_ = sim.readProximitySensor(distanceSensorLeft)
+    leftState, leftDistance, *_ = sim.readProximitySensor(distanceSensorFrontLeft)
+    rightState, rightDistance, *_ = sim.readProximitySensor(distanceSensorFrontRight)
+    right2State, right2Distance, *_ = sim.readProximitySensor(distanceSensorRight)
+
+    # Calculamos la velocidad del robot
+    linearVelocity, _ = sim.getVelocity(My_robot)
+    velocity = (linearVelocity[0]**2 + linearVelocity[1]**2 + linearVelocity[2]**2)**0.5
+    print(f"Velocidad del robot: {velocity}")
+
+    # Verificamos si alguno de los sensores detecta un obstáculo dentro de los 0.2 metros
+    if (frontState == 1 and frontDistance < threshold) or \
+       (leftState == 1 and leftDistance < threshold) or \
+       (rightState == 1 and rightDistance < threshold) or \
+       (right2State == 1 and right2Distance < threshold):
+        
+        print(f"Obstáculo detectado. Distancias a las que se ha detectado: - Frente: {frontDistance}, Izquierda: {leftDistance}, Derecha: {rightDistance}, Derecha2: {right2Distance}")
+
+        # Cambiamos la dirección del robot para que no choque con la pared
+        sim.setJointTargetVelocity(leftMotor, -0.5)  
+        sim.setJointTargetVelocity(rightMotor, 0.5)
+
+        # Avanza  unos pasos de simulación para hacer el giro
+        for _ in range(20):
+            sim.step()
+
+        # Vuelve a avanzar en línea recta después del giro
+        sim.setJointTargetVelocity(leftMotor, 0.5)  
+        sim.setJointTargetVelocity(rightMotor, 0.5)
+
+    #En caso de que no funcionen/no tenga sensores, al dtenerse con la pared, también girará:
+    elif velocity < 0.01:
+        print("Colisión detectada debido a falta de movimiento. Iniciando maniobra de giro.")
+
+        # Cambiamos la dirección del robot para intentar liberarlo del choque
+        sim.setJointTargetVelocity(leftMotor, -0.5)  
+        sim.setJointTargetVelocity(rightMotor, 0.5)
+
+        # Ejecuta unos pasos de simulación para realizar el giro
+        for _ in range(20):
+            sim.step()
+
+        # Vuelve a avanzar en línea recta después del giro
+        sim.setJointTargetVelocity(leftMotor, 0.5)  
+        sim.setJointTargetVelocity(rightMotor, 0.5)
+    
     packed_image, resolution = sim.getVisionSensorImg(camera)
-    raw_image = sim.unpackUint8Table(packed_image, 0, 0)
+    raw_image = sim.unpackUInt8Table(packed_image, 0, 0)
     image = np.array(raw_image, dtype = np.uint8)
     image = image.reshape([resolution[1], resolution[0], 3])
     image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
@@ -53,6 +110,11 @@ while True:
     if key == 27:
         break
 
+    
+    # Imprimimos el estado y sigue la simulación
+    print(f"Estado del sensor frontal: {frontState}, Distancia al obstáculo: {frontDistance}")
+    #avanza más steps de simulación
     sim.step()
+    
 
 sim.stopSimulation()
